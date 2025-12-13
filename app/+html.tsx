b0683169bb58c031ed8a27d08512709cb3a272ad
Gabriel Sobral <ggabrielssobral@gmail.com>
2025-12-12 21:23:38 -0300

    fix: correct typo in comment regarding DOM access

--- app/+html.tsx
+++ app/+html.tsx
@@ -3,7 +3,7 @@ import { ScrollViewStyleReset } from 'expo-router/html';
 // This file is web-only and used to configure the root HTML for every
 // web page during static rendering.
 // The contents of this function only run in Node.js environments and
-// do not have access to the DOM or browser APIs.d
+// do not have access to the DOM or browser APIs.dd
 export default function Root({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
